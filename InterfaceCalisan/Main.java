478d51afcc54dcc4f33504fffa7f51f412d6bf16
urbild <109045540+urbild@users.noreply.github.com>
2023-03-21 17:13:06 +0300

    Update Main.java

--- InterfaceCalisan/Main.java
+++ InterfaceCalisan/Main.java
@@ -4,17 +4,11 @@ public class Main {
     
     
     public static void main(String[] args) {
-        
-        // UdemyInterface.IMuhendis muhendis1 = new UdemyInterface.IMuhendis();
+  
         
         IMuhendis muhendis1 = new PcMuhendisi(false,false);
         
-        /*muhendis1.askerlik_durumu_sorgula();
-        muhendis1.adli_sicil_sorgula();
-        System.out.println(muhendis1.mezuniyet_ortalamasi(3.07));
-        String[] tecrube = {"Vestel","Havelsan","Turksat"};
-        
-        muhendis1.is_tecrubesi(tecrube);*/
+      
         
         MakineMuhendisi muhendis2 = new MakineMuhendisi(true,false);
         
